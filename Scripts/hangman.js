048b4fd2387e482de24be7546c6d8148b5ab6fc7
benjyros <benny.peterhans@gmail.com>
2022-09-15 08:06:30 +0200

    added better js version of hangman

--- Scripts/hangman.js
+++ Scripts/hangman.js
@@ -1,14 +1,118 @@
 $(document).ready(function () {
-    var letter; 
-    var words = [];
+    var words = new Array;
+    var wordArray = new Array;
+    var previousGuesses = new Array;
+    var currentWord;
+    var currentClue;
+    var wrongAnswerCount;
 
-    let step = 0;
+    $.getJSON("../Datas/wordlist.json", function (data) {
+        for (i = 0; i < data.wordlist.length; i++) {
+            words[i] = new Array;
+            words[i][0] = data.wordlist[i].word;
+            words[i][1] = data.wordlist[i].clue;
+        }
+        titleScreen();
+    }) //get json
 
-    const hangman = document.getElementById('hangman').getContext("2d");
+    function titleScreen() {
+        $("#gameContent").append('<div id="gameTitle">HANGMAN</div><div id="startButton" class="button">BEGIN</div>');
+        $("#startButton").on("click", function () {
+            gameScreen();
+        });
+    } //display game
 
-    drawLines();
+    function gameScreen() {
+        $('#gameContent').empty();
+        $('#gameContent').append('<div id="container"><canvas id="hangman" width="180" height="250"></canvas></div>');
+        $('#gameContent').append('<div id="placeholder"></div>');
+        $('#gameContent').append('<div id="clueholder"></div>');
+        $('#gameContent').append('<div id="input"><label for="letter">Letter: </label><input type="text" maxlength="1" id="letter"><button class="button" id="enterLetter">OK</button></div>');
+        $('#gameContent').append('<div id="guesses">Previous guesses:</div>');
+        $('#gameContent').append('<div id="feedback"></div>');
+
+        getWord();
+
+        var numberOfTiles = currentWord.length;
+        wrongAnswerCount = 0;
+        previousGuesses = [];
+
+        for (i = 0; i < numberOfTiles; i++) {
+            $('#placeholder').append('<div class="tile" id=t' + i + '></div>');
+        }
+
+        $('#clueholder').append("HINT: " + currentClue);
+
+        $('#enterLetter').on("click", onBtnClick);
+    } //gamescreen
+
+    function getWord() {
+        var rnd = Math.floor(Math.random() * words.length);
+        currentWord = words[rnd][0];
+        currentClue = words[rnd][1];
+        words.splice(rnd, 1);
+        wordArray = currentWord.split("");
+    } //getword
+
+    function onBtnClick() {
+        var letter = $("#letter").val();
+        $(this).val('');
+        var found = false;
+        var previouslyEntered = false;
+        var input = (letter).toLowerCase();
+
+        for (i = 0; i < previousGuesses.length; i++) {
+            if (input == previousGuesses[i]) {
+                previouslyEntered = true;
+            }
+        }
+
+        if (!previouslyEntered && input != "" && checkLetter(input)) {
+            previousGuesses.push(input);
+
+            for (i = 0; i < wordArray.length; i++) {
+
+                if (input == wordArray[i]) {
+                    found = true;
+                    $('#t' + i).append(input);
+                }
+
+            }
+
+            if (found) {
+                checkAnswer();
+            }
+            else {
+                wrongAnswer(input);
+            }
+        }
+    }
+
+    function checkLetter(letter) {
+        return /^[A-Za-z]*$/.test(letter);
+    }
+
+    function checkAnswer() {
+        var currentAnswer = "";
+        for (i = 0; i < currentWord.length; i++) {
+            currentAnswer += ($('#t' + i).text());
+        }
+        if (currentAnswer == currentWord) {
+            victoryMessage();
+        };
+    } //checkanswer
+
+    function wrongAnswer(a) {
+        wrongAnswerCount++;
+        $('#guesses').append("  " + a);
+        draw(wrongAnswerCount);
+        if (wrongAnswerCount == 10) {
+            defeatMessage();
+        }
+    } //wronganswer
 
     function draw(part) {
+        const hangman = document.getElementById('hangman').getContext("2d");
         switch (part) {
             //bottom
             case (1):
@@ -85,52 +189,30 @@ $(document).ready(function () {
         }
     }
 
-    function drawLines() {
-        placeholder = document.getElementById("placeholder");
-        solution = document.createElement("ul");
+    function victoryMessage() {
+        $('#enterLetter').off("click", onBtnClick);
+        $('#feedback').append("CORRECT!<br><br><div id='replay' class='button'>CONTINUE</div>");
+        $('#replay').on("click", function () {
+            if (words.length > 0) {
+                gameScreen()
+            }
+            else { finalPage() }
+        });
+    } //victory 
 
-        var wordtest = getFile();
-        alert(wordtest + "teeeeeeest");
-        words = getFile().split(',');
-        console.log(words + "hallo0oooo");
-        alert(words);
-        /*
-        let rndNum = Math.floor(Math.random() * words.length);
-        var word = words[rndNum];
+    function defeatMessage() {
+        $('#enterLetter').off("click", onBtnClick);
+        $('#feedback').append("You're Dead!<br>(answer = " + currentWord + ")<div id='replay' class='button'>CONTINUE</div>");
+        $('#replay').on("click", function () {
+            if (words.length > 0) {
+                gameScreen()
+            }
+            else { finalPage() }
+        });
+    } //defeat 
 
-        for (var i = 0; i < word.length; i++) {
-            solution.setAttribute("id", "solution");
-            letter = document.createElement("li");
-            letter.setAttribute("class", "letter");
-            letter.innerHTML = "_";
-            placeholder.appendChild(solution);
-            solution.appendChild(letter);
-        };
-        */
-    }
-
-    function getFile(){
-        var wordlist;
-        $.get("../Data/wordlist.txt", function (data) {
-            wordlist = data;
-            alert(wordlist);
-            return data;
-        }, "text");
-        return wordlist;
-    }
-
-    $("#enterLetter").click(function () {
-        let letter = $("#letter").val();
-
-        if (letter != "" && checkLetter(letter)) {
-            $("#letter").val("");
-            step++;
-            draw(step);
-        }
-    });
-
-    function checkLetter(letter) {
-        return /^[A-Za-z]*$/.test(letter);
-    }
+    function finalPage() {
+        $('#gameContent').empty();
+        $('#gameContent').append('<div id="finalMessage">You have finished all the words in the game!</div>');
+    } //finalpage 
 });
-
